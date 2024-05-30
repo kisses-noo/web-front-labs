ceea91d79b5b836f5ab2bc0c1ac77c1d60ebcd32
Алина Андреичева <alinkakisses@gmail.com>
2024-05-30 20:10:51 +0700

    Вывод текущей даты

---
+++ lab8/lab8.js
@@ -0,0 +1,16 @@
+function showDate() {
+    let out = document.getElementById('current-date');
+    let today = new Date();
+    out.innerHTML += '<br>Россия: ' + today.toLocaleString('ru-RU');
+    out.innerHTML += '<br>США: ' + today.toLocaleString('en-US', { timeZone: 'America/New_York' });
+    out.innerHTML += '<br>Япония: ' + today.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
+    out.innerHTML += '<br>Китай: ' + today.toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' });
+    out.innerHTML += '<br>Бразилия: ' + today.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+    out.innerHTML += '<br>Франция: ' + today.toLocaleString('fr-FR', { timeZone: 'Europe/Paris' });
+
+    out.innerHTML += '<br>Текущий год: ' + today.getFullYear();
+    out.innerHTML += '<br>Текущий месяц: ' + (today.getMonth() + 1);
+    out.innerHTML += '<br>Текущая дата: ' + today.getDate();
+    out.innerHTML += '<br>День недели: ' + today.toLocaleString('ru-RU', { weekday: 'long' });
+
+}
